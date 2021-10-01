1e7dbbbf259601ac5bc0d2418a845ed89041e2d1
leandro-fogaca <leandro-fogaca87@gmail.com>
2021-10-01 14:31:45 -0300

    Project created

---
+++ src/test/java/com/moufog/workshop/WorkshopMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moufog.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
