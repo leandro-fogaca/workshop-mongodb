bc8037f9f859dc1891a59048e6b8b8806412dbd7
leandro-fogaca <leandro-fogaca87@gmail.com>
2021-10-11 11:58:38 -0300

    Post and PostRepository created

--- src/main/java/com/moufog/workshop/config/Instantiation.java
+++ src/main/java/com/moufog/workshop/config/Instantiation.java
@@ -1,14 +1,16 @@
 package com.moufog.workshop.config;
 
-
-
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.moufog.workshop.domain.Post;
 import com.moufog.workshop.domain.User;
+import com.moufog.workshop.repository.PostRepository;
 import com.moufog.workshop.repository.UserRepository;
 
 @Configuration
@@ -16,15 +18,26 @@ public class Instantiation implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
+	@Autowired
+	private PostRepository postRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-
+		
+		Post post1 = new Post(null, sdf.parse("11/10/2021"), "Bom dia!", "Hoje eu acordei feliz!", maria);
+		Post post2 = new Post(null, sdf.parse("10/10/2021"), "Boa noite!", "Hora de dormir", maria);
+		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));	
+		postRepository.saveAll(Arrays.asList(post1, post2));
 
 	}
 
--- src/main/java/com/moufog/workshop/repository/PostRepository.java
+++ src/main/java/com/moufog/workshop/repository/PostRepository.java
@@ -3,10 +3,10 @@ package com.moufog.workshop.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import com.moufog.workshop.domain.User;
+import com.moufog.workshop.domain.Post;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String>{
+public interface PostRepository extends MongoRepository<Post, String>{
 
 
 }
